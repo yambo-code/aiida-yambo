85665527680cc1d6c09d1f213e69182b275ac381
AntimoMarrazzo <antimo.marrazzo@epfl.ch>
2017-10-16 17:43:02 +0200

    Clean wf folder

--- wf/te.py
+++
@@ -1,6 +0,0 @@
-from aiida.parsers.plugins.yambo.ext_dep.yambofile  import  YamboFile
-from aiida.parsers.plugins.yambo.ext_dep.yambofolder  import  YamboFolder
-calc = load_node(2942)
-outf = calc.get_retrieved_node()
-outf.get_abs_path()
-results = YamboFolder(outf.get_abs_path())
