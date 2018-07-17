faa21e004ec3ec2c7302ee10b1a67326027ba8f3
Michael Atambo <michaelontita.atambo@unimore.it>
2018-07-17 17:09:36 +0200

    adding first passing test using a proper aiida inv

--- manage.py
+++ manage.py
@@ -2,10 +2,10 @@
 import unittest
 from aiida.utils.fixtures import TestRunner
 
-tests = unittest.defaultTestLoader.discover('.')
+tests = unittest.defaultTestLoader.discover('./tests')
 result = TestRunner().run(tests, backend='django')
 
 # Note: On travis, this will not fail even if the tests fail.
 # Uncomment the lines below, when aiida 0.12.2 is released to fix this.
-exit_code = int(not result.wasSuccessful())
-sys.exit(exit_code)
+#exit_code = int(not result.wasSuccessful())
+#sys.exit(exit_code)
