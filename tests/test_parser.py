1c109ac44eb91cc0f14f64e9436b1bc0f8a9fb66
Michael Atambo <michaelontita.atambo@unimore.it>
2018-07-17 16:22:28 +0200

    Initial addition for CI tests

---
+++ tests/test_parser.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestRentAmtRecognizer (unittest.TestCase):
+
+    def setUp(self):
+        """
+        It's patching time
+        """
+        pass
+
+    def tearDown(self):
+        """
+        Let's clean up
+        """
+        pass
+
+    def test_simple_log (self):
+        assert False
