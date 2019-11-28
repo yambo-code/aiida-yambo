f69832756ea828a74d906bbcc35ac5d8dc0ad532
Miki Bonacci <mikibonacci@hotmail.it>
2019-11-28 17:04:44 +0100

    d

--- aiida_yambo/workflows/utils/helpers_yambopy.py
+++ aiida_yambo/workflows/utils/helpers_yambopy.py
@@ -23,63 +23,11 @@ class calc_manager_yambopy: #the interface class to AiiDA... could be separated 
         for key in calc_info.keys():
             setattr(self, str(key), calc_info[key])
 
-        try: #AiiDA calculation
-            self.type = str(self.take_down().get_description).split()[-1] #not working now
-        except:
-            #this is not an AiiDA calculation
-            self.type = 'not_AiiDA'
+        pass
 
 ################################## update_parameters #####################################
     def updater(self, inp_to_update, k_distance, first):    #parameter list? yambopy philosophy
-
-        if self.type == 'not_AiiDA':
-            pass
-        else:    #aiida_yambo_conv_updater()...
-            if self.var == 'bands':
-
-                new_params = inp_to_update.yres.gw.parameters.get_dict()
-                new_params['BndsRnXp'][-1] = new_params['BndsRnXp'][-1] + self.delta*first
-                new_params['GbndRnge'][-1] = new_params['GbndRnge'][-1] + self.delta*first
-
-                inp_to_update.yres.gw.parameters = Dict(dict=new_params)
-
-                value = new_params['GbndRnge'][-1]
-
-            elif self.var == 'kpoints':
-
-                k_distance = k_distance + self.delta*first
-
-                inp_to_update.scf.kpoints = KpointsData()
-                inp_to_update.scf.kpoints.set_cell(inp_to_update.scf.pw.structure.cell)
-                inp_to_update.scf.kpoints.set_kpoints_mesh_from_density(1/k_distance, force_parity=True)
-                inp_to_update.nscf.kpoints = inp_to_update.scf.kpoints
-
-                try:
-                    del inp_to_update.parent_folder  #I need to start from scratch...
-                except:
-                    pass
-
-                value = k_distance
-
-            elif self.var == 'cutoff':
-
-                new_params = inp_to_update.yres.gw.parameters.get_dict()
-                new_params['CUTBox'] = new_params['CUTBox'] + [1,1,1]*self.delta*first
-
-                inp_to_update.yres.gw.parameters = Dict(dict=new_params)
-
-                value = new_params['CUTBox'][-1]
-
-            else: #"scalar" quantity
-
-                new_params = inp_to_update.yres.gw.parameters.get_dict()
-                new_params[str(self.var)] = new_params[str(self.var)] + self.delta*first
-
-                inp_to_update.yres.gw.parameters = Dict(dict=new_params)
-
-                value = new_params[str(self.var)]
-
-            return inp_to_update, value
+        pass
 
 ################################## parsers #####################################
     def take_quantities(self, start = 1):
@@ -88,44 +36,14 @@ class calc_manager_yambopy: #the interface class to AiiDA... could be separated 
         where = self.where
         what = self.what
 
-        if self.type == 'not_AiiDA':
-            pass  #yambopy
-
-        else:
-
-            if 'quantumespresso.pw' in self.type:
-                print('from conv_utils.')
-            if 'yambo.yambo' in self.type:
-                print('looking for {} in k-points {}'.format(what,where))
-
-                quantities = np.zeros((len(where),backtrace,3))
-                for j in range(len(where)):
-                    for i in range(1,backtrace+1):
-                        yambo_calc = load_node(self.wfl_pk).caller.called[backtrace-i].called[0].called[0]
-                        if what == 'gap': #datasets from parser????
-                            quantities[j,i-1,1] = abs((yambo_calc.outputs.array_qp.get_array('Eo')[(where[j][1]-1)*2+1]+
-                                        yambo_calc.outputs.array_qp.get_array('E_minus_Eo')[(where[j][1]-1)*2+1]-
-                                        (yambo_calc.outputs.array_qp.get_array('Eo')[(where[j][0]-1)*2]+
-                                        yambo_calc.outputs.array_qp.get_array('E_minus_Eo')[(where[j][0]-1)*2])))
-
-                        if what == 'single-levels':
-                            quantities[j,i-1,1] = yambo_calc.outputs.array_qp.get_array('Eo')[where[j]-1]+ \
-                                        yambo_calc.outputs.array_qp.get_array('E_minus_Eo')[where[j]-1]
-
-                        quantities[j,i-1,0] = i*self.delta  #number of the iteration times the delta... to be used in a fit
-                        quantities[j,i-1,2] = int(yambo_calc.pk) #CalcJobNode.pk
-
-                return quantities
+        pass
 
 #######################################################
     def get_caller(self, calc, depth = 2):
 
         for i in range(depth):
 
-            if self.type == 'not_AiiDA':
-                pass  #yambopy
-            else:
-                calc = load_node(calc).caller
+            pass
 
         return calc
 
@@ -133,20 +51,13 @@ class calc_manager_yambopy: #the interface class to AiiDA... could be separated 
 
         for i in range(depth):
 
-            if self.type == 'not_AiiDA':
-                pass  #yambopy
-            else:
-                calc = load_node(calc).called[0]
+            pass
 
         return calc
 
     def update_converged_parameters(self, node, params):
 
-        if self.type == 'not_AiiDA':
-            pass  #yambopy
-
-        else:
-            self.ctx.calc_inputs.yres.gw.parameters = node.get_builder_restart().yres.gw['parameters']
+        pass
 
 
 ######################### AiiDA specific #############################
