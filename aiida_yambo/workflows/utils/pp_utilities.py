5177979831851861f389f26f55e77abc253bab59
Miki Bonacci <miki.bonacci@unimore.it>
2020-04-02 18:32:34 +0200

    plotting

--- aiida_yambo/workflows/utils/pp_utilities.py
+++ aiida_yambo/workflows/utils/pp_utilities.py
@@ -12,63 +12,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from aiida.orm import load_node
 
-def plot_conv(pk,title='',xlabel='step',ylabel='eV',where=1,physical_quantity='gap',\
-              units={'NGsBlkXp':'Ry','kpoints':'density^-1'}):
+def take_fermi(calc_node_pk):  # calc_node_pk = node_conv_wfl.outputs.last_calculation
 
-    colors = list(matplotlib.colors.TABLEAU_COLORS.items())
+    node = load_node(calc_node_pk)
+    path_folder = node.outputs.retrieved._repository._repo_folder.abspath+'/path/'
+    for i in os.listdir(path_folder):
+        if 'r-aiida.out' in i:
+            file = open(path_folder+i,'r')
+    for line in file:
+        if '[X]Fermi Level' in line:
+            print('The Fermi level is {}'.format(line.split()[3]))
+            ef = line.split()[3]
 
-    x = load_node(pk)
-    y = x.outputs.story.get_list()
-
-    for i in range(len(y)):
-        string=''
-        if isinstance(y[i][y[0].index('var')],list):
-            print(y[i][y[0].index('var')])
-            for k in y[i][y[0].index('var')]:
-                string += k+' & '
-            string= string+'qwerty'
-            string = string.replace('& qwerty','')
-            print(string)
-            y[i][y[0].index('var')]=string
-
-    tot = pd.DataFrame(y[1:],columns=y[0])
-    conv = tot[tot['useful']==True]
-    print(conv,tot)
-    fig,ax = plt.subplots()
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.grid()
-
-    ax.set_title(title)
-    ax.plot(conv['global_step'],np.array(conv['result_eV'].to_list())[:,range(where)],'-',\
-            label='convergence path')
-    for j in range(where):
-        ax.plot(tot['global_step'],np.array(tot['result_eV'].to_list())[:,j],'*--',
-                color='black',label='full path - '+str(tot['where_in_words'].to_list()[0][j]))
-
-    b=[]
-
-    for i in conv['var']:
-        if i not in b:
-            try:
-                unit = units[i]
-            except:
-                unit = ''
-            color = colors[len(b)+where+1][0]
-            act = conv[conv['var']==str(i)]['value'].to_list()
-            print(act)
-            for j in range(where):
-                if j == 0:
-                    label=str(i)+' - '+str(act[-1])+' '+str(unit)
-                else:
-                    label = None
-                ax.plot(conv['global_step'],np.ma.masked_where(np.array(conv['var'].to_numpy()!=str(i)),\
-                            np.array(conv['result_eV'].to_list())[:,j]),'o-' \
-                        ,label=label)
-            b.append(i)
-
-    plt.legend()
-
+    return ef
 
 def collect_results(node_pk):    #returns array (val_1,val_2....,result_eV_1,...) to be further analyzed 
         
@@ -178,43 +134,116 @@ def parse_data(wfl_pk, folder_name='', title='run'):
     
             
     return k
-def plot_2d_results(node,lab = '',title=''):      #just a 3d plot
-    y = load_node(node).outputs.story.get_list()
-    p = pd.DataFrame(y[1:][:],columns = y[0][:])
-    k = np.zeros((len(p),3)) ;
-    for i in range(len(p)):
-         if isinstance(p['value'].iloc[i][0],list):
-            k[i,0] = p['value'].iloc[i][0][1]
-            k[i,1] = p['value'].iloc[i][1][1]
-            k[i,2] = p['result_eV'].iloc[i][0]
-         else:
-            k[i,0] = p['value'].iloc[i][0]
-            k[i,1] = p['value'].iloc[i][1]
-            k[i,2] = p['result_eV'].iloc[i][0]
 
-    matplotlib.rcParams['legend.fontsize'] = 10
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+
+###### PLOT ######
+
+def plot_1D_convergence(pk,title='',xlabel='step',ylabel='eV',where=1,physical_quantity='gap',\
+              units={'NGsBlkXp':'Ry','kpoints':'density^-1'}, save = False):
+
+    colors = list(matplotlib.colors.TABLEAU_COLORS.items())
+
+    x = load_node(pk)
+    y = x.outputs.story.get_list()
+
+    for i in range(len(y)):
+        string=''
+        if isinstance(y[i][y[0].index('var')],list):
+            print(y[i][y[0].index('var')])
+            for k in y[i][y[0].index('var')]:
+                string += k+' & '
+            string= string+'qwerty'
+            string = string.replace('& qwerty','')
+            print(string)
+            y[i][y[0].index('var')]=string
+
+    tot = pd.DataFrame(y[1:],columns=y[0])
+    conv = tot[tot['useful']==True]
+    print(conv,tot)
+    fig,ax = plt.subplots()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.grid()
+
     ax.set_title(title)
-    z = k[:,2]
-    x = k[:,0]
-    y = k[:,1]
-    ax.plot(x, y, z, '-o',label=lab)
-    ax.set_xlabel(p['var'].iloc[1][0])
-    ax.set_ylabel(p['var'].iloc[1][1])
-    ax.legend()
-    plt.show()
+    ax.plot(conv['global_step'],np.array(conv['result_eV'].to_list())[:,range(where)],'-',\
+            label='convergence path')
+    for j in range(where):
+        ax.plot(tot['global_step'],np.array(tot['result_eV'].to_list())[:,j],'*--',
+                color='black',label='full path - '+str(tot['where_in_words'].to_list()[0][j]))
 
-def take_fermi(calc_node_pk):  # calc_node_pk = node_conv_wfl.outputs.last_calculation
+    b=[]
 
-    node = load_node(calc_node_pk)
-    path_folder = node.outputs.retrieved._repository._repo_folder.abspath+'/path/'
-    for i in os.listdir(path_folder):
-        if 'r-aiida.out' in i:
-            file = open(path_folder+i,'r')
-    for line in file:
-        if '[X]Fermi Level' in line:
-            print('The Fermi level is {}'.format(line.split()[3]))
-            ef = line.split()[3]
+    for i in conv['var']:
+        if i not in b:
+            try:
+                unit = units[i]
+            except:
+                unit = ''
+            color = colors[len(b)+where+1][0]
+            act = conv[conv['var']==str(i)]['value'].to_list()
+            print(act)
+            for j in range(where):
+                if j == 0:
+                    label=str(i)+' - '+str(act[-1])+' '+str(unit)
+                else:
+                    label = None
+                ax.plot(conv['global_step'],np.ma.masked_where(np.array(conv['var'].to_numpy()!=str(i)),\
+                            np.array(conv['result_eV'].to_list())[:,j]),'o-' \
+                        ,label=label)
+            b.append(i)
 
-    return ef
+    plt.legend()
+    
+    if save:
+            fig.savefig(str(pk)+'conv.png')
+
+def plot_2D_convergence(xdata, ydata, zdata, labels = {'x_label':'bands','y_label':'Ry','z_label': 'eV'}, title='Gap', plot_type='3D', save = False):      
+        
+    #matplotlib.rcParams['legend.fontsize'] = 10
+    
+    
+    if plot_type=='3D':
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+
+        for i in np.unique(xdata):
+            ind = np.where(xdata==i) 
+            z = zdata[ind]
+            x = xdata[ind]
+            y = ydata[ind]
+            ax.plot(x, y, z, '-o',label= '{} {}'.format(int(i), labels['x_label']))
+    
+        ax.set_title(title)
+        ax.set_xlabel(labels['x_label'])
+        ax.set_ylabel(labels['y_label'])
+        ax.legend()
+        plt.show()
+        
+        if save:
+            fig.savefig(title+'3d.png')
+    
+    elif plot_type=='2D':
+        fig, ax = plt.subplots()
+        for i in np.unique(xdata):
+            ind = np.where(xdata==i)    
+            plt.plot(ydata[ind],zdata[ind],'-o',label='{} {}'.format(int(i),labels['x_label']))
+        ax.set_title(title)
+        ax.set_xlabel(labels['y_label'])
+        ax.set_ylabel(labels['z_label'])
+        ax.legend()
+        plt.show()
+        if save:
+            fig.savefig(title+'2d_1.png')
+        
+        fig, ax = plt.subplots()
+        for i in np.unique(ydata):
+            ind = np.where(ydata==i)    
+            plt.plot(xdata[ind],zdata[ind],'-o',label='{} {}'.format(int(i),labels['y_label']))
+        ax.set_title(title)
+        ax.set_xlabel(labels['x_label'])
+        ax.set_ylabel(labels['z_label'])
+        ax.legend()
+        plt.show()
+        if save:
+            fig.savefig(title+'2d_2.png')
