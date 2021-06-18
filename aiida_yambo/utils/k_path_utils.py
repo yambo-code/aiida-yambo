3d20646d3f29272ce95da01a7e65dcec61ea9795
Miki Bonacci <miki.bonacci@unimore.it>
2021-06-18 09:51:49 +0200

    k recon

--- aiida_yambo/utils/k_path_utils.py
+++ aiida_yambo/utils/k_path_utils.py
@@ -61,7 +61,7 @@ class k_path_dealer():
             ind = 1 
             found = False
             for g in grid:
-                if np.allclose(abs(high_symmetry[point]),abs(g),1e-4,1e-4):
+                if np.allclose(abs(high_symmetry[point]),abs(g),1e-3,1e-3):
                     found = True
                     maps[point] = ind
                     break
@@ -83,7 +83,7 @@ class k_path_dealer():
             ind = 1 
             found = False
             for g in qe_grid:
-                if np.allclose(high_symmetry[point],abs(g),1e-4,1e-4):
+                if np.allclose(abs(high_symmetry[point]),abs(g),1e-3,1e-3):
                     found = True
                     maps[point] = ind
                     break
