5bc8bcbc2fde869644173a47348f2e9ff8488dc7
Miki Bonacci <miki.bonacci@unimore.it>
2021-10-26 22:53:08 +0200

    bf

--- aiida_yambo/utils/common_helpers.py
+++ aiida_yambo/utils/common_helpers.py
@@ -389,11 +389,11 @@ def gap_mapping_from_nscf(nscf_pk, additional_parsing_List=[]):
             dft_predicted = 'semimetal'
     
     L_H = round((min(bands[:,conduction-1])-max(bands[:,valence-1])),3)
-    if L_H<=0.02 and L_H>=-0.02 and fermi <= 0:
+    if L_H<=0.02 and L_H>=-0.02 and valence%2 !== 0:
         dft_predicted = 'semimetal'
-    elif L_H>0.02 and fermi <= 0:
+    elif L_H>0.02 and valence%2 == 0:
         dft_predicted = 'semiconductor/insulator'
-    else fermi>0:
+    elif fermi>0 or valence%2 != 0:
         dft_predicted = 'metal'
 
     if ind_val+1 != ind_cond+1:
