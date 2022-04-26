902671768c4441e0145c89323ed0b73d2001e151
Miki Bonacci <miki.bonacci@unimore.it>
2022-04-26 14:42:23 +0200

    just fixing when doing 2D extra

--- aiida_yambo/workflows/utils/helpers_workflow.py
+++ aiida_yambo/workflows/utils/helpers_workflow.py
@@ -83,7 +83,7 @@ def create_space(starting_inputs={}, workflow_dict={}, calc_dict={}, wfl_type='h
         l = i['var']
         if not isinstance(l,list): l = [l]
         print('SPACE,',space)
-        if hint:
+        if hint and wfl_type != 'newton_2D_extra':
             if hint['new_grid']:
                 new_grid = i['iter']
         if 'new_algorithm_2D' in wfl_type:
